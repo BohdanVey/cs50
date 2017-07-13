c773b198adf161e3b8e157f2f80e1a9960a304bf
Bogdan <bogdan.vey2002@gmail.com>
2017-07-13 18:07:30 +0300

    Зчитує покит менше 0

---
+++ while0morethen0.cpp
@@ -0,0 +1,15 @@
+#include <cs50.h>
+ #include <stdio.h>
+ 
+ // prototype
+ int GetPositiveInt(void);
+ 
+ int main(void)
+ {
+     int n = GetPositiveInt();
+    printf("Thanks for the %i!\n", n)}
+ int GetPositiveInt(void){
+	 int n=1;
+	 while(n>0){
+		 n=GetInt()
+	 }
